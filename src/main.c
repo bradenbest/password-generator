ace5f21a4f252604594f4174dbb17c626054dfcb
Manuel Rigger <manuel.rigger@jku.at>
2017-02-03 18:25:11 +0100

    Check for invalid input parameters

--- src/main.c
+++ src/main.c
@@ -4,13 +4,20 @@
 
 #include "password.h"
 
+#define DEFAULT_LENGTH 4
+
 int main(int argc, char **argv)
 {
   srand(time(0));
-  int len = 4;
-  if(argc > 1)
+  int len;
+  if(argc > 1) {
     len = atoi(argv[1]);
-
+    if (len < 0) {
+      len = DEFAULT_LENGTH;
+    }
+  } else {
+    len = DEFAULT_LENGTH;
+  }
   printf("Password generated: %s\n", gen_password(len));
 
   return 0;
